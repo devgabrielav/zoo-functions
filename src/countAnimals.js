7d2c95964189ced833adafe1c81138916a28e80b
devgabrielav <gabrivicentini@icloud.com>
2023-06-15 18:25:25 -0300

    Nem acredito mas requisito 5 foi!!

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  const novoArray = {};
+  if (animal === undefined) {
+    data.species.forEach((specie) => {
+      novoArray[specie.name] = specie.residents.length;
+    });
+    return novoArray;
+  }
+  if (Object.keys(animal).length === 1) {
+    const quantity = data.species.find(
+      (specie) => specie.name === animal.species,
+    );
+    return quantity.residents.length;
+  }
+  const animals = data.species.find((specie) => specie.name === animal.species);
+  const quantidade = animals.residents.filter(
+    (animale) => animale.sex === animal.sex,
+  );
+  return quantidade.length;
 };
 
 module.exports = countAnimals;
