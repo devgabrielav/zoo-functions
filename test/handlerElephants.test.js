d7f89230384d85cffeb1eca1a5ee84e3618dfd68
devgabrielav <gabrivicentini@icloud.com>
2023-06-15 18:58:55 -0300

    Requisito 6!

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,32 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Testa se handlerElephants retorna undefined', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+  it('Testa se não aceita parametro que não seja string', () => {
+    expect(handlerElephants(5)).toEqual(
+      'Parâmetro inválido, é necessário uma string',
+    );
+  });
+  it('Testa se retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+  it('Testa se retorna os nomes dos elefantes', () => {
+    expect(handlerElephants('names')).toEqual([
+      'Ilana',
+      'Orval',
+      'Bea',
+      'Jefferson',
+    ]);
+  });
+  it('Testa se retorna a média das idades dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+  it('Testa se retorna null', () => {
+    expect(handlerElephants('1223')).toBeNull();
+  });
+  it('Testa se retorna o valor de uma chave', () => {
+    expect(handlerElephants('popularity')).toEqual(5);
+  });
+});
