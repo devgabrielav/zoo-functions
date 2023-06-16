6a007320cd8984d181e148f11a56f786600efda3
devgabrielav <gabrivicentini@icloud.com>
2023-06-16 17:41:41 -0300

    Consegui a 9!! ! Alegria ;)

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,43 @@
 const data = require('../data/zoo_data');
 
+const makeSchedule = () => {
+  const schedule = {};
+  Object.keys(data.hours).forEach((weekDay) => {
+    if (weekDay !== 'Monday') {
+      const { open, close } = data.hours[weekDay];
+      schedule[weekDay] = {
+        officeHour: `Open from ${open}am until ${close}pm`,
+        exhibition: data.species
+          .filter((specie) => specie.availability.includes(weekDay))
+          .map((specie) => specie.name),
+      };
+    }
+    schedule.Monday = {
+      officeHour: 'CLOSED',
+      exhibition: 'The zoo will be closed!',
+    };
+  });
+  return schedule;
+};
+
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  const days = makeSchedule();
+  const specificDay = Object.keys(makeSchedule()).find((day) => day === scheduleTarget);
+  const specificAnimal = data.species.find((specie) => specie.name === scheduleTarget);
+
+  if (specificDay) {
+    const specificSchedule = {};
+    const daySchedule = makeSchedule()[scheduleTarget];
+    specificSchedule[scheduleTarget] = daySchedule;
+    return specificSchedule;
+  }
+  if (specificAnimal) {
+    const especie = data.species.find(
+      (specie) => specie.name === scheduleTarget,
+    );
+    return especie.availability;
+  }
+  return days;
 };
 
 module.exports = getSchedule;
