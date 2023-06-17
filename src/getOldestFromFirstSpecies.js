c5b8149383367963c20adfe91588c3c1d2f9a2ad
devgabrielav <gabrivicentini@icloud.com>
2023-06-17 16:35:53 -0300

    Requisito 10 feito!!

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const oldest = [];
+  const getEmployee = data.employees.find((person) => person.id === id);
+  const findSpecie = getEmployee.responsibleFor.find((cares) => cares);
+  const animals = data.species.find((specie) => specie.id === findSpecie);
+  const animalsR = animals.residents.reduce((a, b) => (a.age > b.age ? a : b));
+  const { name, sex, age } = animalsR;
+  oldest.push(name, sex, age);
+  return oldest;
 };
 
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
