7d5fff3cec41cea0c1eaafaeb64a3af40079dcd9
devgabrielav <gabrivicentini@icloud.com>
2023-06-18 19:53:26 -0300

    Emoção demais, requisito 11 feito!!

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,36 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const geraObjetos = () =>
+  data.employees.map((employee) => {
+    const responsaveis = employee.responsibleFor.filter((id) => id);
+    const objeto = {
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: data.species
+        .filter((animal) => animal.id === responsaveis.find((id) => id === animal.id))
+        .map((animalN) => animalN.name),
+      locations: data.species
+        .filter((animal) => animal.id === responsaveis.find((id) => id === animal.id))
+        .map((animalN) => animalN.location),
+    };
+    return objeto;
+  });
+
+const getEmployeesCoverage = (obj) => {
+  if (obj !== undefined) {
+    const findE = data.employees
+      .find((employeeF) => employeeF.id === obj.id
+    || employeeF.firstName === obj.name
+    || employeeF.lastName === obj.name);
+    if (findE) {
+      const objetos = geraObjetos()
+        .filter((funcionario) => funcionario.fullName.includes(findE.firstName)
+      || funcionario.id === findE.id);
+      return objetos.find((person) => person);
+    }
+    throw new Error('Informações inválidas');
+  }
+  return geraObjetos();
 };
 
 module.exports = getEmployeesCoverage;
